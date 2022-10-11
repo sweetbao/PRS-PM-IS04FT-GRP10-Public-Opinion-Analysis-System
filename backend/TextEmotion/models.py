aae04f77a92c33bc63c539c397ed6d66da4e9977
duwenlei <750811582@qq.com>
2022-10-11 14:49:49 +0800

    data clean func and topic model

--- backend/TextEmotion/models.py
+++ backend/TextEmotion/models.py
@@ -1,15 +1,20 @@
 from pickle import TRUE
 from django.db import models
 
+
 # Create your models here.
 class Tweet(models.Model):
-    title = models.CharField( max_length=100)
-    author = models.CharField( max_length=50)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
     comment = models.TextField()
-    retrievetime = models.DateTimeField( auto_now_add=TRUE)
+    retrievetime = models.DateTimeField(auto_now_add=TRUE)
 
     class Meta:
-        db_table="Twitters"
+        db_table = "Twitters"
 
     def __str__(self):
         return self.name
+
+
+class Topic(models.Model):
+    rank = models.IntegerField()
---
+++ dataClean.py
@@ -0,0 +1,20 @@
+
+
+
+
+def languageCheck():
+
+
+    return True
+
+
+
+def imageRemove(sentence):
+
+
+    return sentence
+
+
+def sentenceClean(sentence):
+
+    return sentence
