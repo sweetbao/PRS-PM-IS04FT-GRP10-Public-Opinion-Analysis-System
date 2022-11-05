df8356870123fcb23dcdcfdf66e56c891b97f252
duwenlei <750811582@qq.com>
2022-11-05 12:21:28 +0800

    change number fetch

--- backend/TextEmotion/service.py
+++ backend/TextEmotion/service.py
@@ -237,7 +237,7 @@ def tweetsGet(trendings):
         print(number)
         set = set_rules(delete, trendings[i], number)
         print('good')
-        target = get_stream(set, 199, number)
+        target = get_stream(set, 999, number)
 
         targetData.append(target)
         print('finish ' + str(trendings[i]))
