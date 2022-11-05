b27326f79cc3a0721295518fc26a8ffbf0d0f858
Daipp <554700142@qq.com>
2022-11-05 13:24:17 +0800

    Removed unused files

--- dBConnect.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-
-db = pymysql.connect(host='localhost',
-                     user='root',
-                     password='Dwl750811582',
-                     database='conversation',
-                     charset='utf8')
-
